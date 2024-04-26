61b5cb4996e5bae79d2b90f942ca56a406ddc844
Paulo19961944 <paulo24041996@gmail.com>
2024-04-26 11:03:43 -0300

    Projeto Oishi - V1.1.0

--- JS/script.js
+++ JS/script.js
@@ -1,64 +1,46 @@
 // Construtor de Data
-const date = new Date() // Chama o construtor de Data
-const yearToday = date.getFullYear() // Captura o Ano Atual
-footerText.innerText = `Direitos de Uso e Copyright | Oishi - ${yearToday}` // Mostra na Tela
+const date = new Date();
+const yearToday = date.getFullYear();
+footerText.innerText = `Direitos de Uso e Copyright | Oishi - ${yearToday}`;
 
 // Manipula Eventos
-const bodyElement = window.document.body // Captura o Body
-const list = document.querySelector('ul')
-menuResponsivo.addEventListener('click', abrirMenu) // Adiciona evento ao botão de Menu no Celular
-fecharMenuMobile.addEventListener('click', fecharMenu) // Adiciona evento ao botão de fechar o Menu no Celular
-document.addEventListener('scroll', getPosition) // Adiciona evento de Scroll a posição da tela
+const bodyElement = document.body;
+menuResponsivo.addEventListener('click', abrirMenu);
+fecharMenuMobile.addEventListener('click', fecharMenu);
+document.addEventListener('scroll', getPosition);
 
 // Função para Abrir o Menu
-function abrirMenu(){
-    bodyElement.style.overflow = 'hidden' // Trava a Tela
-    menuDesktop.classList.add('menu-desktop-celular') // Adiciona a classe do CSS ao Menu
-    menuResponsivo.classList.add('menu-responsivo-celular') // Adiciona classe do CSS ao botão do menu
-    fecharMenuMobile.classList.add('fechar-menu-mobile-celular') // Adiciona classe do CSS ao botão de fechar o menu
+function abrirMenu() {
+    bodyElement.style.overflow = 'hidden';
+    menuDesktop.classList.add('menu-desktop-celular');
+    menuResponsivo.classList.add('menu-responsivo-celular');
+    fecharMenuMobile.classList.add('fechar-menu-mobile-celular');
 }
 
 // Função para fechar o menu
-function fecharMenu(){
-    bodyElement.style.overflow = 'auto' // Reseta o Padrão
-    menuDesktop.classList.remove('menu-desktop-celular') // Remove a classe do CSS ao Menu
-    menuResponsivo.classList.remove('menu-responsivo-celular') // Remove a classe do CSS ao botão de Menu
-    fecharMenuMobile.classList.remove('fechar-menu-mobile-celular') // Remove a classe do CSS ao botão de Fechar o Menu
+function fecharMenu() {
+    bodyElement.style.overflow = 'auto';
+    menuDesktop.classList.remove('menu-desktop-celular');
+    menuResponsivo.classList.remove('menu-responsivo-celular');
+    fecharMenuMobile.classList.remove('fechar-menu-mobile-celular');
 }
 
 // Captura a Posição
-function getPosition(){
-    const whatsappBtn = document.getElementById('whatsapp') // Armazena o botão do Whatsapp em uma variável
-    const mainElement = document.querySelector('main') // Armazena o Main em uma Variável
-    const footerElement = document.querySelector('footer') // Armazena o Footer em uma Variável
-    const mainRect = mainElement.getBoundingClientRect() // Captura a posição do Main
-    const footerRect = footerElement.getBoundingClientRect() // Captura a posição do Footer
-    const isDesktop = window.innerWidth >= 1024
-    const isEndPositionMain = footerRect.top <= window.innerHeight && mainRect.bottom > footerRect.height
-    const footerRectHeightDesktop = footerRect.height - 179
-    const footerRectHeightMobile = footerRect.height - 148
+function getPosition() {
+    const whatsappBtn = document.getElementById('whatsapp');
+    const mainElement = document.querySelector('main');
+    const footerElement = document.querySelector('footer');
+    const mainRect = mainElement.getBoundingClientRect();
+    const footerRect = footerElement.getBoundingClientRect();
+    const isDesktop = window.innerWidth >= 1024;
+    const isEndPositionMain = footerRect.top <= window.innerHeight && mainRect.bottom > footerRect.height;
+    const footerRectHeightDesktop = footerRect.height - 179;
+    const footerRectHeightMobile = footerRect.height - 148;
 
-    // Se for Desktop
-    if(isDesktop){
-        // Se o footer estiver no topo e o main estiver embaixo
-        if(isEndPositionMain){
-            whatsappBtn.style.position = 'absolute' // Muda a posição para absoluta
-            whatsappBtn.style.bottom = footerRectHeightDesktop + 'px' // Corrige o Botão do Whatsapp
-            whatsappBtn.style.right = '24px' // Botão do Whatsapp a direita com uma margem de 24px
-        } else{
-            whatsappBtn.style.position = 'fixed' // Reseta a posição
-            whatsappBtn.style.bottom = '0px' // Reseta a posição
-            whatsappBtn.style.right = '24px' // Reseta a posição
-        }
-    }  else {
-        if(isEndPositionMain){
-            whatsappBtn.style.position = 'absolute' // Muda a posição para absoluta
-            whatsappBtn.style.bottom = footerRectHeightMobile + 'px' // Corrige o botão do Whatsapp
-            whatsappBtn.style.right = '8px' // Ajusta o botão a direita em 24px
-        } else{
-            whatsappBtn.style.position = 'fixed' // Reseta a posição
-            whatsappBtn.style.bottom = '0px' // Reseta a posição
-            whatsappBtn.style.right = '8px' // Reseta a posição
-        }
-    } 
+    const positionStyle = isEndPositionMain ? 'absolute' : 'fixed';
+    const bottomValue = isEndPositionMain ? (isDesktop ? footerRectHeightDesktop : footerRectHeightMobile) + 'px' : '0px';
+
+    whatsappBtn.style.position = positionStyle;
+    whatsappBtn.style.bottom = bottomValue;
+    whatsappBtn.style.right = isDesktop ? '24px' : '8px';
 }
